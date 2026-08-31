0d0627de7a5e081b914de0cecaf57147a9b7fda7
solmazkarimics50x <140386087+solmazkarimics50x@users.noreply.github.com>
2026-08-31 18:26:52 +0430

    Initial commit: Uploading FinalPythonProject_LMS_SolmazKarimi project with all folders, scripts, backup, modules, video, and zip

--- PythonFinallProjectSolmazwithAdminNonAdmin/Model/UserModule.py
+++
@@ -1,8 +0,0 @@
-class UserModel:
-    def __init__(self,user_name,password,firstname:str=None,lastname:str=None,isadmin:bool=False):
-        self.UserName = user_name
-        self.Password = password
-        self.FirstName = firstname
-        self.LastName = lastname
-        self.IsAdmin = isadmin
-
